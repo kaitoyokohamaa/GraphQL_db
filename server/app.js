9dd3c02d7f0efa8e8ed17d79d55d6f95894e0aa1
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-25 17:44:24 +0900

    connect mongodb

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require("mongoose")
 const app = express();
 
+mongoose.connect("mongodb+srv://cluster0.sgx5v.mongodb.net/<kaito>", { useNewUrlParser: true })
+mongoose.connection.once("open",()=>{
+    console.log("connected to database")
+})
+mongoose.set('useCreateIndex', true);
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,13 +3,12 @@ const graphql = require("graphql");
 const _ = require("lodash")
 
 
-const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLInt,GraphQLID}=graphql
+const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLInt,GraphQLID,GraphQLList}=graphql
 // dummy data
 var books = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1' ,authorId:"1"},
     { name: 'The Final Empire', genre: 'Fantasy', id: '2' ,authorId:"2"},
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3',authorId:"3" },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
     { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
     { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
@@ -43,6 +42,12 @@ const AuthorType = new GraphQLObjectType({
         id:{type:GraphQLString},
         name:{type:GraphQLString},
         age:{type:GraphQLInt},
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return _.filter(books,{ authorId:parent.id})
+            }
+        }
     })
 }) 
  
@@ -64,6 +69,18 @@ const RootQuery= new GraphQLObjectType({
     resolve(parent,args){
         return _.find(authors,{id:args.id})
     }
+    },
+    books:{
+        type:new GraphQLList(BookType),
+        resolve(parent,args){
+            return books
+        }
+    },
+    authors:{
+        type:new GraphQLList(AuthorType),
+        resolve(parent,args){
+            return authors
+        }
     }
     }
 })
